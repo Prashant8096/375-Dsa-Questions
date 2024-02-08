307b9cbfc76178cf5391035c95d600d82d17e9a2
Prashant Rathore <pp454460@gmail.com>
2024-02-08 20:55:00 +0530

    commit

---
+++ minmaxarrayele.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Pair{
+    int min;
+    int max;
+};
+ Pair getminmax(int arr[] , int n){
+    Pair minmax;
+    sort(arr, arr+n);
+    minmax.min=arr[0];
+    minmax.max=arr[n-1];
+    return minmax;
+ }
+ int main(){
+    int arr[]={100,11,445,1,330,3000};
+    int arr_size=sizeof(arr)/sizeof(arr[0]);
+    Pair minmax=getminmax(arr,arr_size);
+    cout<<"Minimum element is = "<<minmax.min<<endl;
+    cout<<"Maximum element is = "<<minmax.max<<endl;
+    return 0;
+ }
---
+++ reversearray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void reverseArrayExtraArray(int arr[],int size){
+    int reversedArr[size];
+    for(int i=0; i<size; i++){
+        reversedArr[i]=arr[size-i-1];
+    }
+    cout<<"Reversed Array = ";
+    for(int i=0;i<size;i++){
+        std::cout<<reversedArr[i]<<" ";
+    }
+}
+int main(){
+    int originalArr[]={2,3,4,5,6};
+    int size=sizeof(originalArr)/sizeof(originalArr[0]);
+    reverseArrayExtraArray(originalArr,size);
+}
